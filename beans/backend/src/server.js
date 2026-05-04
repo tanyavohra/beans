bc6ff3aa843a406aaa07c221eacb8f28bf6c1909
tanyavohra <vohratanya5@gmail.com>
2026-05-04 15:44:33 +0530

    #

--- beans/backend/src/server.js
+++ beans/backend/src/server.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+const path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const { createApp } = require("./app");
 const { connectDb } = require("./db");
